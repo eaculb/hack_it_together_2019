ae91183f80d125f078b8711ff05595b86a7c03ac
lynoska <lynoskagarcia@mail.adelphi.edu>
2019-03-17 22:55:32 -0400

    list with snacks and drinks offered inflight to be used in /requests

---
+++ snacks_drinks.py
@@ -0,0 +1,30 @@
+SNACKS_LIST = [
+	"Cheez-It Crackers", 
+	"Rold Gold Pretzels", 
+	"PopCorners PopCorn Chips",
+	"Belgian Boys Choc O'Chips Cookies",
+	"TERRA Sweets & Blues potato chips"
+	]
+
+DRINKS_LIST = [
+	"Coke",
+	"Diet Coke",
+	"Coke Zero",
+	"Sprite",
+	"Sprite Zero",
+	"Dasani Bottled Water",
+	"Fuze Ice Tea",
+	"Ocean Spray Pineapple Juice",
+	"Ocean Spray Apple Juice",
+	"Ocean Spray Cranberry Juice",
+	"Ocean Spray Orange Juice",
+	"Campbell's Tomato Juice",
+	"Mr. & Mrs. T Bloody Mary Mix",
+	"Seagram's Ginger Ale",
+	"Seagram's Seltzer Water",
+	"Seagram's Tonic Water",
+	"Dunkin' Original Blend Coffee",
+	"Dunkin' Decaf",
+	"Dunkin Black Tea",
+	"Green Tea"
+	]
