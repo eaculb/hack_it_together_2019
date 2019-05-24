085de9127dc3437694cdbd2a5878bbe7b6ffa971
Lynoska <lynoskagarcia@mail.adelphi.edu>
2019-05-24 00:52:38 -0400

    added check for description box and alerts. Need to fix this

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,4 @@
-function isEmptySubmition() {
+function isEmptySubmition(name, seat) {
 	var selected = false;
 
 	// checks to see if a drink was selected
@@ -17,6 +17,20 @@ function isEmptySubmition() {
 	for(var j=1; j<4; j++) {
 		if(document.getElementById("purchase-" + j).checked == true)
 			selected = true;
+	}
+
+	// checks to see if client requested something else
+	if(document.getElementById("other-request").length != 0) {
+		selected = true;
+	}
+
+
+	if(selected == true) {
+		alert("Your request has been submitted, " + name + " ! \nSomeone will be \
+			over in seat " + seat + " soon!");
+	}
+	else {
+		alert("You are trying to submit an empty form. Please make a selection.");
 	}
 
 	return selected;
